c3a008ad559110f290ec5965acd511ff93235f61
Paulo F. Oliveira <paulo.ferraz.oliveira@gmail.com>
2020-11-17 23:46:00 +0000

    Adapt to :get_stacktrace deprecation

--- src/ktn_debug.erl
+++ src/ktn_debug.erl
@@ -3,6 +3,12 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 -module(ktn_debug).
 
+-ifdef(OTP_RELEASE).
+-define(GET_STACKTRACE, []).
+-else.
+-define(GET_STACKTRACE, erlang:get_stacktrace()).
+-endif.
+
 -export(
   [ ppst/0
   , ppst/1
@@ -11,7 +17,7 @@
 -spec ppst() ->
   [any()].
 ppst() ->
-  ppst(erlang:get_stacktrace()).
+  ppst(?GET_STACKTRACE).
 
 -spec ppst([any()]) ->
   [any()].
