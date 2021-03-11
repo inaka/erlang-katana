2a8e6059b8c7dcb63d8bf324e12993a5a33cfa58
Pehuen <pehuen.rodriguez@nextroll.com>
2021-03-11 16:13:05 -0300

    Add some tests to ktn_task

---
+++ test/ktn_task_SUITE.erl
@@ -0,0 +1,84 @@
+-module(ktn_task_SUITE).
+
+-export([
+         all/0,
+         init_per_testcase/2,
+         end_per_testcase/2
+        ]).
+
+-export([
+         task/1,
+         task_error/1
+        ]).
+
+-define(EXCLUDED_FUNS,
+        [
+          module_info,
+          all,
+          init_per_case,
+          end_per_case
+        ]).
+
+-type config() :: [{atom(), term()}].
+-type fail() :: {fail, {integer(), integer()}}.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Common test
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+-spec all() -> [atom(), ...].
+all() ->
+    Exports = ?MODULE:module_info(exports),
+    [F || {F, 1} <- Exports, not lists:member(F, ?EXCLUDED_FUNS)].
+
+-spec init_per_testcase(term(), config()) -> config().
+init_per_testcase(_Case, Config) ->
+    ets:new(?MODULE, [named_table, public]),
+    ets:insert(?MODULE, {counter, counters:new(1, [write_concurrency])}),
+    Config.
+
+-spec end_per_testcase(term(), config()) -> config().
+end_per_testcase(_Case, Config) ->
+    ets:delete(?MODULE),
+    Config.
+
+% Took this idea from
+% https://gist.github.com/garazdawi/17cdb5914b950f0acae21d9fcf7e8d41
+-spec cnt_incr() -> ok.
+cnt_incr() ->
+    counters:add(
+        ets:lookup_element(?MODULE, counter, 2), 1, 1).
+
+-spec cnt_read() -> integer().
+cnt_read() ->
+    counters:get(
+        ets:lookup_element(?MODULE, counter, 2), 1).
+
+-spec fails_until(integer()) -> fail() | ok.
+fails_until(X) ->
+    fails_until(X, cnt_read()).
+
+-spec fails_until(integer(), integer()) -> fail() | ok.
+fails_until(X, Curr) when X > Curr ->
+    cnt_incr(),
+    throw({fail, {Curr, X}});
+fails_until(_X, _Curr) ->
+    ok.
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% Test Cases
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+-spec task(config()) -> ok.
+task(_Config) ->
+    ok = ktn_task:wait_for(fun() -> ok end, ok, 1, 1),
+    ok = ktn_task:wait_for(fun() -> fails_until(10) end, ok, 1, 11),
+    ok.
+
+-spec task_error(config()) -> ok.
+task_error(_Config) ->
+    {error, {timeout, {fail}}} =
+              ktn_task:wait_for(fun() -> throw({fail}) end, 1, 1, 3),
+    {error, {timeout, {fail, {10, 15}}}} =
+              ktn_task:wait_for(fun() -> fails_until(15) end, ok, 1, 11),
+    ok.
